35eeaa0412d0504ca37f1f6c8a9a4ba9428419a1
Brooke Wittenberg <brooke.wittenberg@gmail.com>
2017-10-20 15:07:22 -0500

    customer table added

--- bangazon-api/config/routes.rb
+++ bangazon-api/config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  resources :customers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get "customers" => "customers#index"
+  post "customers" => "customers#create"
+  get "customers/:id" => "customers#show"
+  patch "customers/:id" => "customers#update"
+  put "customers/:id" => "customers#update"
 end
+
