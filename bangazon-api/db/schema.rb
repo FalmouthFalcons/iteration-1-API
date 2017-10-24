4f24f17f6373d84421f77d06d9cd1648a1ec78ef
Malcolm Ford <malcolmf38@gmail.com>
2017-10-24 10:59:51 -0500

    The seed file is created

--- bangazon-api/db/schema.rb
+++ bangazon-api/db/schema.rb
@@ -11,6 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20171023205358) do
+
   create_table "computers", force: :cascade do |t|
     t.date "purchase_date"
     t.date "decomission_date"
--- bangazon-api/db/seeds.rb
+++ bangazon-api/db/seeds.rb
@@ -1,7 +1,61 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Computer.create!([
+  {purchase_date: "2017-02-22", decomission_date: "2017-05-13"},
+  {purchase_date: "2017-02-22", decomission_date: "2017-05-13"},
+  {purchase_date: "2017-02-22", decomission_date: "2017-05-13"}
+])
+Customer.create!([
+  {last_name: "Ford", first_name: "Malcolm", date_customer_created: nil, active: true, last_date_used: nil, email: "brooke.wittenberg@gmail.com", street_address: "1414 Monetta Ave", city: "Nashville", us_state: "TN", zip_code: 37216},
+  {last_name: "Ford", first_name: "Malcolm", date_customer_created: nil, active: true, last_date_used: nil, email: "brooke.wittenberg@gmail.com", street_address: "1414 Monetta Ave", city: "Nashville", us_state: "TN", zip_code: 37216},
+  {last_name: "Ford", first_name: "Malcolm", date_customer_created: nil, active: true, last_date_used: nil, email: "brooke.wittenberg@gmail.com", street_address: "1414 Monetta Ave", city: "Nashville", us_state: "TN", zip_code: 37216}
+])
+Department.create!([
+  {department_name: "HR", expense_budget: 2000.0},
+  {department_name: "HR", expense_budget: 2000.0},
+  {department_name: "HR", expense_budget: 2000.0}
+])
+Employee.create!([
+  {name_last: "Ford", name_first: "Malcolm", job_title: "Junior Dev", employee_hire_date: "2016-04-20", Department_id: 1},
+  {name_last: "Ford", name_first: "Malcolm", job_title: "Junior Dev", employee_hire_date: "2016-04-20", Department_id: 1},
+  {name_last: "Ford", name_first: "Malcolm", job_title: "Junior Dev", employee_hire_date: "2016-04-20", Department_id: 1}
+])
+EmployeeComputer.create!([
+  {employee_id: 1, computer_id: 1},
+  {employee_id: 1, computer_id: 1},
+  {employee_id: 1, computer_id: 1}
+])
+EmployeeTrainingProgram.create!([
+  {employee_id: 1, training_program_id: 3},
+  {employee_id: 1, training_program_id: 3},
+  {employee_id: 1, training_program_id: 3}
+])
+Order.create!([
+  {Customer_id: 1, Payment_Type_id: 1},
+  {Customer_id: 1, Payment_Type_id: 1},
+  {Customer_id: 1, Payment_Type_id: 1}
+])
+OrderProduct.create!([
+  {product_id: 1, order_id: 1},
+  {product_id: 1, order_id: 1},
+  {product_id: 1, order_id: 1}
+])
+PaymentType.create!([
+  {payment_type: "DebtCard3000", account_number: 123456789, Customer_id: 1},
+  {payment_type: "DebtCard3000", account_number: 123456789, Customer_id: 1},
+  {payment_type: "DebtCard3000", account_number: 123456789, Customer_id: 1}
+])
+Product.create!([
+  {product_title: "t-shirt", product_price: 6.99, Customer_id: 1},
+  {product_title: "t-shirt", product_price: 6.99, Customer_id: 1},
+  {product_title: "t-shirt", product_price: 6.99, Customer_id: 1}
+])
+ProductType.create!([
+  {product_type: "clothing"},
+  {product_type: "hat"},
+  {product_type: "pants"},
+  {product_type: "food"}
+])
+TrainingProgram.create!([
+  {start_date: "2017-04-25", end_date: "2017-03-29", subject: "customer service", max_occupancy: 28},
+  {start_date: "2017-12-25", end_date: "2018-01-29", subject: "customer service", max_occupancy: 28},
+  {start_date: "2017-12-25", end_date: "2018-01-29", subject: "customer service", max_occupancy: 28}
+])
